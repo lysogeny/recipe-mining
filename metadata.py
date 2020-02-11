40ad829f25293a5a098b71426584834e056d466e
Jooa <eru@jooa.xyz>
2020-02-11 23:14:11 +0100

    Add comment and category outputs

--- metadata.py
+++ metadata.py
@@ -2,6 +2,7 @@
 
 """Make metadata csv"""
 
+import os
 import argparse
 
 import pandas as pd
@@ -16,27 +17,64 @@ def get_ingredients(entry):
     base = {'ingredient': ingredient, 'quantity': quantity}
     meta = {key: entry[key]
             for key in entry
-            if key not in ['instructions', 'comments', 'ingredients']}
+            if key not in ['instructions', 'comments', 'ingredients', 'categories']}
+    base.update(meta)
+    return pd.DataFrame(base)
+
+def get_comments(entry):
+    """Gets dataframe of ingredients for recipe"""
+    comments = entry['comments']
+    base = {}
+    for comment in comments:
+        for key in comment:
+            if key not in base:
+                base[f"comment_{key}"] = []
+            base[f"comment_{key}"].append(comment[key])
+    # need to turn a list of dicts into a dict of lists
+    meta = {key: entry[key]
+            for key in entry
+            if key not in ['instructions', 'comments', 'ingredients', 'categories']}
+    base.update(meta)
+    return pd.DataFrame(comments)
+
+def get_categories(entry):
+    """Gets dataframe of ingredients for recipe"""
+    categories = entry['categories']
+    base = {'category': categories}
+    meta = {key: entry[key]
+            for key in entry
+            if key not in ['instructions', 'comments', 'ingredients', 'categories']}
     base.update(meta)
     return pd.DataFrame(base)
 
 def main():
     """Main function"""
     parser = argparse.ArgumentParser(description="Concatenate recipes into metadata")
-    parser.add_argument("input", nargs="+", type=str, help="input files")
+    parser.add_argument("input", nargs="+", type=str, help="input folder")
     parser.add_argument("-o", "--output", type=str, help="Output recipes to file")
     parser.add_argument("-i", "--ingredients", type=str, help="Output ingredients to file")
+    parser.add_argument("-c", "--categories", type=str, help="Output categories to file")
+    parser.add_argument("-m", "--comments", type=str, help="Output comments to file")
     args = parser.parse_args()
-    data = map(recipes.read_recipe, args.input)
+    inputs = [f"{args.input[0]}{os.path.sep}{i}" for i in os.listdir(args.input[0])]
+    data = map(recipes.read_recipe, inputs)
     data = list(data)
     if 'output' in args:
         outdata = pd.DataFrame(data)
-        outdata.drop(["comments", "ingredients"], axis=1).to_csv(args.output, index=False)
+        outdata.drop(["categories", "comments", "ingredients"], axis=1).to_csv(args.output, index=False)
     if 'ingredients' in args:
         # get ingredients and put into file
         ingredients = map(get_ingredients, data)
         dataframes = map(pd.DataFrame, ingredients)
         pd.concat(dataframes).to_csv(args.ingredients, index=False)
+    if 'categories' in args:
+        categories = map(get_categories, data)
+        dataframes = map(pd.DataFrame, categories)
+        pd.concat(dataframes).to_csv(args.categories, index=False)
+    if 'comments' in args:
+        comments = map(get_comments, data)
+        dataframes = map(pd.DataFrame, comments)
+        pd.concat(dataframes).to_csv(args.comments, index=False)
 
 #files =[f"json/{i}" for i in os.listdir("json")]
 #data = map(recipes.read_recipe, files)
